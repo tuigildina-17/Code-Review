a955b4df59ffa8f7dbdc1437811225b0b222201c
tuigildina-17 <tujgildinad@gmail.com>
2026-03-31 17:16:06 +0500

    Add files via upload

---
+++ Лабораторная 1/Задача 3/code-review_1.3/code-review_1.3/Header.h
@@ -0,0 +1,18 @@
+/*  - ичные числа.—реди чисел в системе счислени€ с основанием K(2<=K<=10)
+определить сколько имеетс€ чисел из N(1 < N < 20, N + K < 26) разр€дов 
+таких, что в их записи не содержитс€ более трех подр€д идущих нулей.ƒл€ того,
+чтобы избежать переполнени€, ответ представьте в виде вещественного числа.*/
+
+#pragma once
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <Windows.h>
+//FIX_ME: Google Style запрещает использование using namespace std;
+// using namespace std;
+
+//FIX_ME: »м€ функции не соответствует стилю
+//bool NoThreeZeros(const string& num);
+bool no_three_zeros(const std::string& num);
+//FIX_ME: ѕодсчет количества чисел находилс€ в main
+double count_valid_numbers(int k, int n);
---
+++ Лабораторная 1/Задача 3/code-review_1.3/code-review_1.3/code-review_1.3.cpp
@@ -0,0 +1,59 @@
+﻿#include "Header.h"
+
+/*FIX_ME:
+* 1. Название функции не соответствует стилю
+* 2. Открывающиеся скобки на отдельной строке
+* 3. Использование using namespace std;
+* 4. Использование табуляции
+*/
+int main() {
+  SetConsoleCP(1251);
+  SetConsoleOutputCP(1251);
+
+  int k, n;
+
+  while (true) {
+        /*cout << "Введите основание системы счисления K(2 <= K <= 10): ";
+        cin >> k;*/
+    std::cout << "Введите основание системы счисления K(2 <= K <= 10): ";
+    std::cin >> k;
+        //FIX_ME: Неполноценная проверка корректности ввода
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(10000, '\n');
+      std::cout << "Ошибка: введите число.\n";
+      continue;
+    }
+    if (k >= 2 && k <= 10)
+      break;
+
+        //cout << "Некорректный ввод. Повторите попытку." << endl;
+    std::cout << "Некорректный ввод. Повторите попытку.\n";
+  }
+
+  while (true) {
+        /*cout << "Введите кол-во разрядов N(1 < N < 20): ";
+        cin >> n;*/
+    std::cout << "Введите кол-во разрядов N(1 < N < 20): ";
+    std::cin >> n;
+        //FIX_ME: Неполноценная проверка корректности ввода
+    if (std::cin.fail()) {
+      std::cin.clear();
+      std::cin.ignore(10000, '\n');
+      std::cout << "Ошибка: введите число.\n";
+      continue;
+    }
+    if (n > 1 && n < 20 && (n + k) < 26)
+      break;
+        //cout << "Некорректный ввод. Повторите попытку." << endl;
+      std::cout << "Некорректный ввод. Повторите попытку.\n";
+    }
+
+    //double total = CountValidNumbers(k, n);
+  double total = count_valid_numbers(k, n);
+
+    //cout << "Количество чисел: " << total << endl;
+  std::cout << "Количество чисел: " << total << "\n";
+
+  return 0;
+}
